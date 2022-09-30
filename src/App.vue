67b2cbf2c343853478bad5bed518aa6ac9d7b807
muyi <lu13813616548@qq.com>
2022-10-01 04:02:00 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-      <router-view></router-view>
+    <router-view></router-view>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
-
 <script>
 
 export default {
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
 import router from '@/router'
 import store from '@/store'
 import '@/styles/index.less'
+
+Vue.use(Vant)
+
 
 Vue.config.productionTip = false
 
