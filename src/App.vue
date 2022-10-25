dc9327e285f7ecfa88da395d85c3c4af18a46cf2
muyi <lu13813616548@qq.com>
2022-10-25 19:21:05 +0800

    ceshi

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <router-view></router-view>
     <van-button type="primary">主要按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
   </div>
 </template>
 <script>
